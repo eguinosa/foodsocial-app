24f393647ad97adf4ef012f3a8ae03f6bf074f7b
Gelin Eguinosa <eguinosa@gmail.com>
2023-08-02 16:22:47 -0400

    Move Home widget to the Home class.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // 2023
 
 import 'package:flutter/material.dart';
+import 'home.dart';
 import 'foodsocial_theme.dart';
 
 void main() {
@@ -13,29 +14,15 @@ class FoodSocial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // [Done] TODO: Create theme
+    // Create theme
     final theme = FoodSocialTheme.dark();
-    // TODO: Apply Home Widget
+
+    // Apply Home Widget
     return MaterialApp(
-      // [Done] TODO: Add theme.
+      // Add theme.
       theme: theme,
       title: 'Food Social',
-      home: Scaffold(
-        // [Done] TODO: Style the title.
-        appBar: AppBar(
-          title: Text(
-            'Food Social',
-            style: theme.textTheme.headline6,
-          ),
-        ),
-        // [Done] TODO: Style the body text.
-        body: Center(
-          child: Text(
-            "Let's get cooking 👩‍🍳",
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
